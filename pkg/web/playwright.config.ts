6962d48846cb3776d305cefb0a4e9750d5a3867b
Luma <world@luma.email>
2025-06-23 00:41:41 +0900

    cc task done: manage/tasks-done/2025-06-22/intro-storybook.txt

---
+++ pkg/web/playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src',
+  testMatch: '**/*.stories.@(js|jsx|ts|tsx)',
+  use: {
+    baseURL: 'http://localhost:6006',
+  },
+  webServer: {
+    command: 'npm run storybook',
+    port: 6006,
+    reuseExistingServer: !process.env.CI,
+  },
+});
